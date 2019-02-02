d167dc299572ac932f77bf83850f5e89a836fb9a
kim1893 <kim1893@purdue.edu>
2019-02-02 15:00:40 -0500

    Using node package Spotify

--- node-backend/routes/index.js
+++ node-backend/routes/index.js
@@ -5,7 +5,7 @@ router.get('/', (req, res) => {
     res.send({success: 'true', message: 'Connected to SpotDB server.'})
 });
 
-var params = router.get('/login', spotify.login);
-//console.log(params.stack[0].route)
+router.get('/login', spotify.login);
+router.get('/search', spotify.search);
 router.post('/getaccess', spotify.getaccess);
 module.exports = router;
--- node-backend/routes/spotify.js
+++ node-backend/routes/spotify.js
@@ -1,10 +1,15 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 var spotify_client_id = process.env.SPOTIFY_API_ID;
 var spotify_client_secret = process.env.SPOTIFY_API_SECRET;
-//var spoitfy_oauth_token = require('spotdb-react-app/src/pages/login');
-//console.log(spoitfy_oauth_token)
+var spotify = new SpotifyWebApi({
+    id: spotify_client_id,
+    secret: spotify_client_secret,
+    redirectUri: 'http://localhost:3000/authorized'
+  });
 
 var login = (req, res) => {
     var scopes = 'user-read-private user-read-email';
@@ -21,13 +26,14 @@ var getaccess = (req, res) => {
 }
 
 var search = (req, res) => {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        headers: {
-            'Authorization': 'Bearer ' + spoitfy_oauth_token
-        }, 
-
-    })
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log(data); 
+      });
+    
 }
 
 module.exports = {
