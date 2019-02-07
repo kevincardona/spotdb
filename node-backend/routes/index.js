ac395722cb5f4aebc0412a567a70838d8776da4b
kevincardona <kevincardona1@gmail.com>
2019-02-07 17:58:42 -0500

    style updates + beginning mongo

--- node-backend/routes/index.js
+++ node-backend/routes/index.js
@@ -1,10 +1,10 @@
 var router      = require('express').Router();
 var spotify     = require('./spotify');
+var user       = require('./user');
 
 router.get('/', (req, res) => {
     res.send({success: 'true', message: 'Connected to SpotDB server.'})
 });
-
 router.get('/login', spotify.login);
 router.post('/getaccess', spotify.getaccess);
 module.exports = router;
