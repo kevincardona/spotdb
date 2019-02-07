2f13e8654f7368b88e4fcc0b51322a0cfe268aff
Kevin Cardona <kevincardona1@gmail.com>
2019-02-07 18:10:20 -0500

    merged files

--- spotdb-react-app/src/pages/Login.js
+++ spotdb-react-app/src/pages/Login.js
@@ -2,36 +2,7 @@ import React from 'react';
 import '../layouts/Login.css'
 import spotifyLogo from '../assets/Spotify_Icon_RGB_Green.png';
 
-<<<<<<< HEAD
-export default class Login extends Component {
-
-    getHashParams() {
-      var hashParams = {};
-      var e, r = /([^&;=]+)=?([^&;]*)/g,
-          q = window.location.hash.substring(1);
-      e = r.exec(q)
-      while (e) {
-         hashParams[e[1]] = decodeURIComponent(e[2]);
-         e = r.exec(q);
-      }
-      return hashParams;
-    }
-
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <a href='http://localhost:5000/login'> Login to Spotify </a>
-          </header>
-        </div>
-      );
-    }
-  }
-=======
 class Login extends React.Component {
-	// constructor() {
-	// 	super();
-	// }
 	getHashParams() {
 		var hashParams = {};
 		var e, r = /([^&;=]+)=?([^&;]*)/g,
@@ -61,4 +32,3 @@ class Login extends React.Component {
 }
 
 export default Login;
->>>>>>> origin/master
