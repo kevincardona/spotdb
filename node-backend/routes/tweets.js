e0420aa9ec6bbca91d36dd20cc1ffaf8d0c052bc
Gus Vroustouris <gvrousto@purdue.edu>
2019-02-21 12:31:59 -0500

    access tweets from mongo with node

--- node-backend/routes/tweets.js
+++ node-backend/routes/tweets.js
@@ -1,17 +1,15 @@
-var mongoose    = require('mongoose');
-var config      = require('../config');
+var mongoose      = require('mongoose');
+var config        = require('../config');
+const ArtistModel = require('../models/artist.js').Artist;
 
 
 var getTweets = (req, res) => {
   console.log('here');
   mongoose.connect(config.mongo_url);
   var db = mongoose.connection;
-  console.log(mongoose.connection.db);
-  console.log(mongoose.connection.artists);
-  db.once('open', function() {
-    return res.json({success: true, message: "Connected to mongoose"});
-
-  });
+  ArtistModel.find({}, (err, data) =>{
+    res.send(data);
+  })
 };
 
 var functions = {
