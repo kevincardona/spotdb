3c141cfbf2af7f1b6fefb92d448dbe36f68dcc19
kevincardona <kevincardona1@gmail.com>
2019-02-21 21:53:15 -0500

    finished all of my sprint 1 stories

--- node-backend/config.js
+++ node-backend/config.js
@@ -2,4 +2,6 @@ module.exports = {
     port: process.env.PORT || 5000,
     mongo_url: process.env.MONGO_URL || 'mongodb://localhost:27017/spotdb',
     geocodio_key: 'c6cdc66262525000492221a9c14c6f151c94296',
+    sock_port: 8000,
+    secret: process.env.secret || 'tHs1sS0secRet' //This will be moved to .env for builds
 }
--- node-backend/routes/index.js
+++ node-backend/routes/index.js
@@ -7,13 +7,18 @@ router.get('/', (req, res) => {
     res.send({success: 'true', message: 'Connected to SpotDB server.'})
 });
 router.get('/login', spotify.login);
-router.post('/authorized', spotify.authorized);
 router.get('/search', spotify.search);
-router.get('/getaddress', util.getAddress); //Gets lat/lon in query and returns address
+router.get('/getaddress', util.getAddress);
+router.post('/authorized', user.authorize);
 
-// Requires User to Be Logged In
+
+// Anything below requires user to be logged in
+router.use(user.authenticate);
+router.get('/authenticate', (req,res) => { return res.json({success: true, authenticated: true})})
 router.get('/accountinfo', spotify.accountInfo)
 router.get('/getHome', user.getHome);
 router.post('/sethome', user.setHome);
+router.get('/listening', spotify.listening);
+router.post('/currentlisteners', spotify.currentListeners);
 
 module.exports = router;
