c41e275e2e659c9d2d5e13ad82945bc0dd955f5e
kevincardona <kevincardona1@gmail.com>
2019-02-27 09:57:11 -0500

    moving config variables to .env

--- node-backend/config.js
+++ node-backend/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: process.env.PORT || 5000,
-    mongo_url: process.env.MONGO_URL || 'mongodb://spotdb-admin:SuperSecr3tPasswordb@ds249035.mlab.com:49035/spotdb',
-    geocodio_key: 'c6cdc66262525000492221a9c14c6f151c94296',
+    mongo_url: process.env.MONGO_URL || 'mongodb://localhost:27017/spotdb',
+    geocodio_key: process.env.GEOCODIO_KEY,
     sock_port: 8000,
     secret: process.env.secret || 'tHs1sS0secRet' //This will be moved to .env for builds
 }
