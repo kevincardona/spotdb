4272430749bd039838d6125737f4a60c61d40f8e
kevincardona <kevincardona1@gmail.com>
2019-02-27 10:22:37 -0500

    fixed heroku .env expectations

--- node-backend/config.js
+++ node-backend/config.js
@@ -1,7 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 module.exports = {
     port: process.env.PORT || 5000,
-    mongo_url: process.env.MONGO_URL || 'mongodb://localhost:27017/spotdb',
-    geocodio_key: process.env.GEOCODIO_KEY,
+    mongo_url: process.env.MONGODB_URI,
+		geocodio_key: process.env.GEOCODIO_KEY,
     sock_port: 8000,
     secret: process.env.secret || 'tHs1sS0secRet' //This will be moved to .env for builds
 }
