d2375f2caa5b833a898531ddb121c2294114def9
Kevin Cardona <kevincardona@Kevins-iMac.lfytina2.metronetinc.net>
2019-03-19 15:00:18 -0400

    fixing mismatched variables

--- node-backend/config.js
+++ node-backend/config.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
     port: process.env.PORT || 5000,
     mongo_url: process.env.MONGODB_URI,
-		geocodio_key: process.env.GEOCODIO_KEY,
+	geocodio_key: process.env.GEOCODIO_KEY,
     sock_port: 8000,
     secret: process.env.secret || 'tHs1sS0secRet' //This will be moved to .env for builds
 }
