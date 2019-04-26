9bb590526e1805e7947891829da9ba1b1ab7f817
Gus Vroustouris <gvrousto@purdue.edu>
2019-04-26 11:43:36 -0400

    removed print statements

--- node-backend/routes/database.js
+++ node-backend/routes/database.js
@@ -3,13 +3,10 @@ var config        = require('../config');
 const AddArtistModel = require('../models/addArtist.js').AddArtist;
 
 var addArtistToDB = (req, res) => {
-  console.log(req.body.name);
   mongoose.connect(config.mongo_url);
   AddArtistModel.find({name: req.body.name}, (err, data) =>{
-    console.log(data.length);
     if(data.length == 0){
       let addArtist = new AddArtistModel({name: req.body.name});
-      console.log('should add');
       addArtist.save(function (err) {
         if (err) return handleError(err);
         // saved!
